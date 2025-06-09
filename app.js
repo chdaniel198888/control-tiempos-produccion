d4cdc8af82ce4621526e0e90d8f12bc8bedad4f4
chdaniel198888 <gerencia@chiosburger.com>
2025-06-08 20:29:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -521,6 +521,7 @@ const TiempoProduccionApp = () => {
       } else {
         console.log('✅ Guardado en tabla Ejecución');
       }
+      */
       
       // 2. Guardar en nueva tabla Registro_Etapas_Ejecutadas
       const datosEtapa = {
